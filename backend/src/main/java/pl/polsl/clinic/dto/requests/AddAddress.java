72d40ddfe5ec168bf917523954ae40fd1917ae96
Patryk Niesporek <pn312381@student.polsl.pl>
2026-04-03 15:57:08 +0200

    PN: Dtos for transporting Patient, Doctor data

---
+++ backend/src/main/java/pl/polsl/clinic/dto/requests/AddAddress.java
@@ -0,0 +1,25 @@
+package pl.polsl.clinic.dto.requests;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import pl.polsl.clinic.entity.Address;
+
+@NotNull
+public record AddAddress(@NotNull @Size(min = 1, max = 100) String city,
+                         @NotNull @Size(min = 1, max = 100) String street,
+                         @NotNull Integer houseNo,
+                         Integer apartmentNo) {
+
+	public Address mapToEntity() {
+		Address address = new Address();
+		populateAddressFields(address);
+		return address;
+	}
+
+	public void populateAddressFields(Address address) {
+		address.setCity(city);
+		address.setStreet(street);
+		address.setHouseNo(houseNo);
+		address.setApartmentNo(apartmentNo);
+	}
+}
