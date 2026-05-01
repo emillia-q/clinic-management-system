2055623fb7e27e7d348defd183ed8c163c444c50
Patryk Niesporek <pn312381@student.polsl.pl>
2026-05-01 11:44:29 +0200

    PN: added missing getById and missing documentation

--- backend/src/main/java/pl/polsl/clinic/controller/VisitController.java
+++ backend/src/main/java/pl/polsl/clinic/controller/VisitController.java
@@ -10,11 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import pl.polsl.clinic.dto.common.error.InvalidParametersErrorDetails;
 import pl.polsl.clinic.dto.common.error.ItemNotFoundErrorDetails;
 import pl.polsl.clinic.dto.visit.request.CreateVisitRequest;
 import pl.polsl.clinic.dto.visit.request.UpdateVisitRequest;
 import pl.polsl.clinic.dto.visit.response.VisitDto;
+import pl.polsl.clinic.entity.Visit;
 import pl.polsl.clinic.enums.VisitStatus;
+import pl.polsl.clinic.exception.ItemNotFoundException;
 import pl.polsl.clinic.service.VisitService;
 
 import java.time.LocalDate;
@@ -30,6 +33,8 @@ public class VisitController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Add a new visit to the system")
+	@ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = InvalidParametersErrorDetails.class))})
 	public VisitDto create(@RequestBody @Valid CreateVisitRequest req) {
 		return visitService.createVisit(req);
 	}
@@ -39,7 +44,7 @@ public class VisitController {
 	@Operation(summary = "Get a list of visits(appointments) that match the parameters. Ordered by appointment date ascending.")
 	@ApiResponse(responseCode = "200", description = "List of matching visits", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = VisitDto.class)))})
 	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
-	public Iterable<VisitDto> getAll(
+	public Iterable<VisitDto> getAllMatching(
 		@RequestParam(required = false) Long doctorId,
 		@RequestParam(required = false) Long patientId,
 		@RequestParam(required = false) LocalDate fromDate, //on date (fromDate===toDate)
@@ -53,17 +58,38 @@ public class VisitController {
 			.map(VisitDto::fromEntity).toList();
 	}
 
+	@GetMapping("{id}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Get the visit details by id")
+	@ApiResponse(responseCode = "200", description = "Visit details", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = VisitDto.class)))})
+	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
+	public VisitDto getById(
+		@PathVariable Long id) {
+		return VisitDto.fromEntity(visitService.getById(id).orElseThrow(() -> new ItemNotFoundException(Visit.class, id)));
+	}
+
 	@PatchMapping("/{id}/cancel")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Cancel a visit due to reasons")
+	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
 	public VisitDto cancel(@PathVariable Long id) {
 		return visitService.updateStatus(id, VisitStatus.Cancelled);
 	}
 
 	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Update a visit")
+	@ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = InvalidParametersErrorDetails.class))})
+	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
 	public VisitDto update(@PathVariable Long id, @RequestBody @Valid UpdateVisitRequest req) {
 		return visitService.updateVisit(id, req);
 	}
 
 	@GetMapping("/patient/{patientId}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Get patient visits. Ordered by appointment date ascending. (left for backwards compatibility, use /api/v1/visits instead)")
+	@ApiResponse(responseCode = "200", description = "List of patient visits", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = VisitDto.class)))})
+	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
 	public List<VisitDto> getHistory(@PathVariable Long patientId) {
 		var params = VisitService.VisitParams.builder()
 			.patientId(patientId)
