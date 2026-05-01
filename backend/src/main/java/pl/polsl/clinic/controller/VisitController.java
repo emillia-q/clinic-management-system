08681d1d77bbd02d53d9c4d96c1053356c3f7647
Patryk Niesporek <pn312381@student.polsl.pl>
2026-05-01 11:27:05 +0200

    PN: removed duplicated methods and cleaned up unused methods

--- backend/src/main/java/pl/polsl/clinic/controller/VisitController.java
+++ backend/src/main/java/pl/polsl/clinic/controller/VisitController.java
@@ -1,16 +1,25 @@
 package pl.polsl.clinic.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import pl.polsl.clinic.dto.common.error.ItemNotFoundErrorDetails;
 import pl.polsl.clinic.dto.visit.request.CreateVisitRequest;
-import pl.polsl.clinic.dto.visit.response.VisitDto;
-import pl.polsl.clinic.service.VisitService;
-import pl.polsl.clinic.enums.VisitStatus;
-import java.util.List;
 import pl.polsl.clinic.dto.visit.request.UpdateVisitRequest;
+import pl.polsl.clinic.dto.visit.response.VisitDto;
+import pl.polsl.clinic.enums.VisitStatus;
+import pl.polsl.clinic.service.VisitService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,9 +35,22 @@ public class VisitController {
 	}
 
 	@GetMapping
-	//TODO: change to use getMatchingVisits() instead to reduce redundancy
-	public List<VisitDto> getAll(@RequestParam(required = false) VisitStatus status) {
-		return visitService.getAllVisits(status);
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Get a list of visits(appointments) that match the parameters. Ordered by appointment date ascending.")
+	@ApiResponse(responseCode = "200", description = "List of matching visits", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = VisitDto.class)))})
+	@ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ItemNotFoundErrorDetails.class))})
+	public Iterable<VisitDto> getAll(
+		@RequestParam(required = false) Long doctorId,
+		@RequestParam(required = false) Long patientId,
+		@RequestParam(required = false) LocalDate fromDate, //on date (fromDate===toDate)
+		@RequestParam(required = false) LocalDate toDate,
+		@RequestParam(required = false) VisitStatus status) {
+		var params = VisitService.VisitParams.builder()
+			.doctorId(doctorId).patientId(patientId).fromDate(fromDate).toDate(toDate).status(status)
+			.build();
+		return StreamSupport
+			.stream(visitService.getMatchingVisits(params).spliterator(), false)
+			.map(VisitDto::fromEntity).toList();
 	}
 
 	@PatchMapping("/{id}/cancel")
@@ -43,6 +65,11 @@ public class VisitController {
 
 	@GetMapping("/patient/{patientId}")
 	public List<VisitDto> getHistory(@PathVariable Long patientId) {
-		return visitService.getPatientVisitHistory(patientId);
+		var params = VisitService.VisitParams.builder()
+			.patientId(patientId)
+			.build();
+		return StreamSupport
+			.stream(visitService.getMatchingVisits(params).spliterator(), false)
+			.map(VisitDto::fromEntity).toList();
 	}
 }
--- backend/src/main/java/pl/polsl/clinic/repository/VisitRepository.java
+++ backend/src/main/java/pl/polsl/clinic/repository/VisitRepository.java
@@ -3,12 +3,6 @@ package pl.polsl.clinic.repository;
 import org.springframework.stereotype.Repository;
 import pl.polsl.clinic.entity.Visit;
 
-import pl.polsl.clinic.enums.VisitStatus;
-import java.util.List;
-
 @Repository
 public interface VisitRepository extends SpecificationJpaRepository<Visit, Long> {
-	List<Visit> findByStatus(VisitStatus status);
-
-	List<Visit> findByPatientPatientIdOrderByAppointmentDateDesc(Long patientId);
 }
