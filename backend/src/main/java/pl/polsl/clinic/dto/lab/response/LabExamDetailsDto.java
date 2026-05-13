934ec4d22fa12385c744a725bc83ea9a05226136
Patryk Niesporek <pn312381@student.polsl.pl>
2026-05-13 18:30:12 +0200

    added null safety to LabExamDetailsDto and more fields.

--- backend/src/main/java/pl/polsl/clinic/dto/lab/response/LabExamDetailsDto.java
+++ backend/src/main/java/pl/polsl/clinic/dto/lab/response/LabExamDetailsDto.java
@@ -1,11 +1,15 @@
 package pl.polsl.clinic.dto.lab.response;
 
 import pl.polsl.clinic.entity.LabExam;
+import pl.polsl.clinic.entity.LabManager;
+import pl.polsl.clinic.entity.LabTechnician;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public record LabExamDetailsDto(
 	Long id,
+	String examCode,
 	String examName,
 	String patientName,
 	String patientPesel,
@@ -15,21 +19,36 @@ public record LabExamDetailsDto(
 	String status,
 	String result,
 	String doctorNotes,
-	String managerNotes
+	String managerNotes,
+	Long labTechnicianId,
+	Long labManagerId,
+	Long doctorId,
+	Long patientId
 ) {
 	public static LabExamDetailsDto fromEntity(LabExam entity) {
+		var patient = entity.getVisit().getPatient();   //not nullable
+		var doctor = entity.getVisit().getDoctor();     //not nullable
 		return new LabExamDetailsDto(
 			entity.getLabExamId(),
+			entity.getExamDict().getExamCode(),
 			entity.getExamDict().getExamName(),
-			entity.getVisit().getPatient().getFirstName() + " " + entity.getVisit().getPatient().getLastName(),
-			entity.getVisit().getPatient().getSocialSecurityNo(),
-			entity.getVisit().getDoctor().getFirstName() + " " + entity.getVisit().getDoctor().getLastName(),
+			patient.getFirstName() + " " + patient.getLastName(),
+			patient.getSocialSecurityNo(),
+			doctor.getFirstName() + " " + doctor.getLastName(),
 			entity.getOrderDate(),
 			entity.getExecutionCancelDate(), // Cancellation or execution date
 			entity.getStatus(),
 			entity.getResult(),
 			entity.getDoctorNotes(),
-			entity.getManagerNotes()
+			entity.getManagerNotes(),
+			Optional.ofNullable(entity.getLabTechnician())
+				.map(LabTechnician::getUserId)
+				.orElse(null),
+			Optional.ofNullable(entity.getLabManager())
+				.map(LabManager::getUserId)
+				.orElse(null),
+			doctor.getUserId(),
+			patient.getPatientId()
 		);
 	}
 }
