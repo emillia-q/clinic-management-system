8ea2747a0b8d644a1e3806cad3b765c3da53a5e3
Patryk Niesporek <pn312381@student.polsl.pl>
2026-05-14 09:37:36 +0200

    feat: both lab technician and manager are logged when updating status

--- backend/src/main/java/pl/polsl/clinic/controller/LabTechnicianController.java
+++ backend/src/main/java/pl/polsl/clinic/controller/LabTechnicianController.java
@@ -1,11 +1,15 @@
 package pl.polsl.clinic.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.polsl.clinic.dto.lab.response.LabExamDto;
+import pl.polsl.clinic.entity.Doctor;
 import pl.polsl.clinic.enums.LabExamStatus;
+import pl.polsl.clinic.exception.ItemNotFoundException;
+import pl.polsl.clinic.security.jwt.JwtService;
 import pl.polsl.clinic.service.LabService;
 
 import java.util.List;
@@ -15,6 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1/lab-technician")
 public class LabTechnicianController {
 	private final LabService labService;
+	private final JwtService jwtService;
 
 	@GetMapping("/exams/pending")
 	@Operation(summary = "Get list of ordered exams")
@@ -25,14 +30,16 @@ public class LabTechnicianController {
 	@PatchMapping("/exams/{id}/result")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@Operation(summary = "Submit result (Execute exam)")
-	public void submitResult(@PathVariable Long id, @RequestBody String result) {
-		labService.submitResult(id, result);
+	public void submitResult(@PathVariable Long id, @RequestBody String result, HttpServletRequest request) {
+		var jwt = jwtService.getTokenFromRequest(request).orElseThrow(() -> new ItemNotFoundException(Doctor.class, "Auth Header missing"));
+		labService.submitResult(id, result, jwtService.extractUserId(jwt));
 	}
 
 	@PatchMapping("/exams/{id}/cancel")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@Operation(summary = "Cancel laboratory exam")
-	public void cancel(@PathVariable Long id) {
-		labService.cancelExam(id);
+	public void cancel(@PathVariable Long id, HttpServletRequest request) {
+		var jwt = jwtService.getTokenFromRequest(request).orElseThrow(() -> new ItemNotFoundException(Doctor.class, "Auth Header missing"));
+		labService.cancelExam(id, jwtService.extractUserId(jwt));
 	}
 }
